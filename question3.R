0205fe793e1ba03528143b03e437f19514ee2bef
yrahan <yrahan@users.noreply.github.com>
2014-05-10 13:57:22 +0200

    Question 3 solved.

--- question3.R
+++ question3.R
@@ -1,20 +1,17 @@
 library(xlsx)
 
-# create directory to store data
-dataDirName <- "data"
-if (!file.exists(dataDirName)){dir.create(dataDirName)}
-
 # download data
-dataFilePath <- paste(".", dataDirName, "mydata.xlsx", sep="/")
-fileUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
-download.file(fileUrl, destfile=dataFilePath, method="curl")
-#
-# read downloaded file
-#mydata <- read.xlsx(dataFilePath, sheetIndex=1, header=FALSE)
 
-# example of reading only a subset
-colIndex <- 18:23
-rowIndex <- 7:15
-read.xlsx(dataFilePath, sheetIndex=1, header=TRUE,
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileUrl, destfile="mydata.xlsx", method="curl")
+
+# read only a subset
+colIndex <- 7:15
+rowIndex <- 18:23
+
+dat <- read.xlsx("mydata.xlsx", sheetIndex=1, header=TRUE,
                  colIndex=colIndex, rowIndex=rowIndex, stringsAsFactors=FALSE)
-#sum(dat$Zip*dat$Ext,na.rm=T) 
+
+sum(dat$Zip*dat$Ext,na.rm=T) 
+
+# 36534720
