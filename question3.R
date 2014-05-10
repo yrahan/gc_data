d6da62e82dd74b0f37e02e508ac9c2a6be3dab57
yrahan <yacine.bourkeb@gmail.com>
2014-05-10 11:18:54 +0200

    try solve question3

--- question3.R
+++ question3.R
@@ -6,15 +6,15 @@ if (!file.exists(dataDirName)){dir.create(dataDirName)}
 
 # download data
 dataFilePath <- paste(".", dataDirName, "mydata.xlsx", sep="/")
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
-download.file(fileUrl, destfile=dataFilePath, method="curl", mode="wb")
-
+fileUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileUrl, destfile=dataFilePath, method="curl")
+#
 # read downloaded file
-mydata <- read.xlsx(dataFilePath, sheetIndex=1, header=FALSE)
+#mydata <- read.xlsx(dataFilePath, sheetIndex=1, header=FALSE)
 
 # example of reading only a subset
 colIndex <- 18:23
 rowIndex <- 7:15
-dat <- read.xlsx(dataFilePath, sheetIndex=1, header=TRUE,
-                 colIndex=colIndex, rowIndex=rowIndex)
+read.xlsx(dataFilePath, sheetIndex=1, header=FALSE,
+                 colIndex=colIndex, rowIndex=rowIndex, stringsAsFactors=FALSE)
 #sum(dat$Zip*dat$Ext,na.rm=T) 
