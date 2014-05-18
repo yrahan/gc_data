19b3231be6e1a5b2adba4579ef6fd49357c377d2
yrahan <yacine.bourkeb@gmail.com>
2014-05-18 14:59:51 +0200

    quizz done.

--- question1.R
+++ question1.R
@@ -1,14 +1,22 @@
-# Quiz 2.
-# Problem 1.
 library(httr)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
 oauth_endpoints("github")
-# 
-myapp <- oauth_app("github", 
-                   "", 
-                   "")
+
+# 2. Register an application at https://github.com/settings/applications
+#    Insert your values below - if secret is omitted, it will look it up in
+#    the GITHUB_CONSUMER_SECRET environmental variable.
+#
+#    Use http://localhost:1410 as the callback url
+myapp <- oauth_app("github", "your-20-digits", secret="your-very-long-hex-number")
+
+# 3. Get OAuth credentials
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
 req <- GET("https://api.github.com/rate_limit", config(token = github_token))
 stop_for_status(req)
 content(req)
-# curl -u Access Token:x-oauth-basic "https://api.github.com/users/jtleek/repos"
+
 BROWSE("https://api.github.com/users/jtleek/repos",authenticate("Access Token","x-oauth-basic","basic"))
