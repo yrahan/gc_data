4f638970834795d413322fb2b916f010a6f1da00
yrahan <yacine.bourkeb@gmail.com>
2014-05-27 11:17:07 +0200

    quizz done.

---
+++ quizz.R
@@ -0,0 +1,115 @@
+# Question 1
+
+# The American Community Survey distributes downloadable data about United States communities.
+# Download the 2006 microdata survey about housing for the state of Idaho using download.file() 
+# from here: 
+#   
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv 
+# 
+# and load the data into R. The code book, describing the variable names is here: 
+#   
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+
+# Get and load data
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileUrl,destfile="./data/communities.csv",method="curl")
+communities <- read.csv("./data/communities.csv")
+
+# Apply strsplit() to split all the names of the data frame on the characters "wgtp". 
+# What is the value of the 123 element of the resulting list?
+
+names(communities)
+?strsplit
+strsplit(names(communities), "wgtp")[[123]]
+
+# Question 2
+
+# Load the Gross Domestic Product data for the 190 ranked countries in this data set: 
+#   
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv 
+# 
+# Remove the commas from the GDP numbers in millions of dollars and average them. What is the average? 
+# 
+# Original data sources: http://data.worldbank.org/data-catalog/GDP-ranking-table
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+download.file(fileUrl,destfile="./data/gdp.csv",method="curl")
+gdp <- read.csv("./data/gdp.csv",header=FALSE, skip=5,nrows=215,stringsAsFactors=FALSE)
+
+names(gdp)
+head(gdp)
+tail(gdp, n=10)
+mygdp <- as.numeric(gsub(",", "", gdp$V5))
+
+mean(mygdp, na.rm=TRUE)
+
+
+# Question 3
+# 
+# In the data set from Question 2 what is a regular expression that would allow you 
+# to count the number of countries whose name begins with "United"? 
+# Assume that the variable with the country names in it is named countryNames. 
+# How many countries begin with United?
+countryNames <- gdp$V4
+grep("^United",countrynames, value=TRUE)
+
+# Question 4
+# Load the Gross Domestic Product data for the 190 ranked countries in this data set: 
+#   
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv 
+# 
+# Load the educational data from this data set: 
+#   
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv 
+# 
+# Match the data based on the country shortcode. 
+# Of the countries for which the end of the fiscal year is available, how many end in June? 
+# 
+# Original data sources: 
+#   http://data.worldbank.org/data-catalog/GDP-ranking-table 
+# http://data.worldbank.org/data-catalog/ed-stats
+# 
+# 
+
+library(plyr)
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+download.file(fileUrl,destfile="./data/gdp.csv",method="curl")
+gdp <- read.csv("./data/gdp.csv",header=FALSE, skip=5,nrows=215,stringsAsFactors=FALSE)
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+download.file(fileUrl,destfile="./data/education.csv",method="curl")
+education <- read.csv("./data/education.csv",stringsAsFactors=FALSE)
+
+
+
+gdp <- rename(gdp, c("V1"="CountryCode", "V2"="Ranking", "V4"="Name", "V5"="GDP"))
+head(gdp, n=3)
+
+gdp_education <- merge(gdp, education, by.x="CountryCode", by.y="CountryCode", all=TRUE)
+names(gdp_education)
+grep("[Jj]une",gdp_education$Special.Notes, value=TRUE)
+grep("Fiscal year end: June",gdp_education$Special.Notes, value=TRUE)
+
+
+
+# Question 5
+# You can use the quantmod (http://www.quantmod.com/) package 
+# to get historical stock prices for publicly traded companies on the NASDAQ and NYSE. Use the following code to download data on Amazon's stock price and get the times the data was sampled.
+# library(quantmod)
+# amzn = getSymbols("AMZN",auto.assign=FALSE)
+# sampleTimes = index(amzn) 
+# 
+# How many values were collected in 2012? How many values were collected on Mondays in 2012?
+
+library(quantmod)
+amzn = getSymbols("AMZN",auto.assign=FALSE)
+sampleTimes = index(amzn) 
+
+library(lubridate)
+length(grep("2012", sampleTimes))
+addmargins(table(year(sampleTimes), weekdays(sampleTimes)))
